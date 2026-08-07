76be6d9013c83b6b8e635c5934bc7bcaf045bf9b
Christian Evangelista <307418105+Chan-DE-hub@users.noreply.github.com>
2026-08-07 21:49:05 +0800

    feat: add Gold dimensions and business aggregations

---
+++ src/enterprise_lakehouse/gold/dimensions/__init__.py
@@ -0,0 +1,9 @@
+"""Gold dimension transformations."""
+
+from enterprise_lakehouse.gold.dimensions.customer_dimension import (
+    CustomerDimensionTransformer,
+)
+
+__all__ = [
+    "CustomerDimensionTransformer",
+]
